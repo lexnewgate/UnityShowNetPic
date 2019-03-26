1dbbc542751303e061bbcf6dc8695ba2f1c7cd1a
lexnewgate <lexnewgate@gmail.com>
2019-03-27 00:50:08 +0800

    test abort

--- NetPicDemo/Assets/DownloadTexture.cs
+++ NetPicDemo/Assets/DownloadTexture.cs
@@ -10,46 +10,77 @@ using UnityEngine.UI;
 public class DownloadTexture : MonoBehaviour
 {
 
-    public Image image;
-    public RawImage rawImage;
+    public RawImage image1;
+    public RawImage image2;
 
+    async void Load0()
+    {
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
+        var op=www.SendWebRequest();
+        www.Abort();
+        Debug.Log($"load0:{op.isDone}");
+
+       
+    }
+
+    private UnityWebRequest www;
+    async void Load1()
+    {
+
+        www = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
+        await www.SendWebRequest();
+        Debug.Log("1");
+        image1.texture = ((DownloadHandlerTexture) www.downloadHandler).texture;
+    }
+
+    async void Load2()
+    {
+        UnityWebRequest www1 = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
+        await www1.SendWebRequest();
+        Debug.Log("2");
+        image2.texture = ((DownloadHandlerTexture) www1.downloadHandler).texture;
+    }
 
     async  void Start()
     {
-        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
-        await www.SendWebRequest();
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-            //image.sprite = myTexture;
-            //image.image = myTexture;
-            rawImage.texture = myTexture;
+        Load0();
+        Load1();
+        Load2();
+        www.Abort();
 
-        }
+                //if (www.isNetworkError || www.isHttpError)
+        //{
+        //    Debug.Log(www.error);
+        //}
+        //else
+        //{
+        //    Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        //    //image.sprite = myTexture;
+        //    //image.image = myTexture;
+        //    rawImage.texture = myTexture;
+
+        //}
     }
 
-    IEnumerator GetTexture()
-    {
-        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
-        yield return www.SendWebRequest();
+    //IEnumerator GetTexture()
+    //{
+    //    UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553619317321&di=f80e6005e936268893a407f3e61ba6fa&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201502%2F02%2F20150202223641_SirzA.thumb.700_0.jpeg");
+    //    yield return www.SendWebRequest();
 
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-            //image.sprite = myTexture;
-            //image.image = myTexture;
-            rawImage.texture = myTexture;
+    //    if (www.isNetworkError || www.isHttpError)
+    //    {
+    //        Debug.Log(www.error);
+    //    }
+    //    else
+    //    {
+    //        Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+    //        //image.sprite = myTexture;
+    //        //image.image = myTexture;
+    //        rawImage.texture = myTexture;
 
-        }
-    }
+    //    }
+    //}
 }
 
 public static class XAwaitExtensions
